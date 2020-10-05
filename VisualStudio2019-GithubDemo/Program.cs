255300a649ef2db7656f2347807d386c8c1e53a1
Vaibhav Pagare <Vaibhav Pagare@DESKTOP-M9P5LU6>
2020-10-05 16:55:28 +0530

    Initial commit

---
+++ VisualStudio2019-GithubDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019_GithubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
